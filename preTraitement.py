a5c247dc9d222f7e5c91355275493129abb86d2c
Simou12 <117384981+Simou12@users.noreply.github.com>
2023-03-13 12:49:48 +0100

    MAJ

--- preTraitement.py
+++ preTraitement.py
@@ -1,6 +1,16 @@
+import nltk as nltk
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import sys
+from nltk.corpus import stopwords
+nltk.download('stopwords')
 
-df=pd.read_csv("data/testFile.csv",sep='\t')
+dfTest=pd.read_csv("data/testFile.csv",sep=',')
+dfTrain=pd.read_csv("data/trainFile.csv",sep=',')
+dfConcat=pd.concat([dfTest,dfTrain],axis=0,ignore_index=True)
+#Supprimer les lignes qui n'ont pas de titres
+dfConcat=dfConcat.dropna(how='any')
+listStopWords=set(stopwords.words("english"))
+print(listStopWords)
+
